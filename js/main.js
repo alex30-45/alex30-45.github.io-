22b014b886dcdd7f45ad085d54116038cc198446
alex30-45 <71391741+alex30-45@users.noreply.github.com>
2021-05-17 13:58:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.slider').slick({
+    slidesToShow: 1,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  autoplaySpeed: 6000,
+    dots: false,
+    speed: 2000,
+    arrows: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+    });
+  });
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
